55fd8b7a3f9c80997ec46b36a9d50665b9305890
dariopedroni_dev <dario.pedroni80@gmail.com>
2020-12-31 17:31:36 +0100

    Small update.

--- input2Language.py
+++ input2Language.py
@@ -1,17 +1,39 @@
-# Input box for selecting language. Numbered value automatically converted to integer varaible.
 
-class lanInput:
+
+def lanInput():
     lanInt = int(input("Language/Lingua/Språk: \n 1.English \n 2.Italiano \n 3.Norsk \n"))
-    if lanInt == 1:
-        lanVal = 'en'
-    if lanInt == 2:
-        lanVal = 'it'
-    if lanInt == 3:
-        lanVal = 'no'
-    if lanInt >= 4:
-        print('Invalid number!')
+    while lanCheck == 0:
+
+        if lanInt == 1:
+            lanVal = 'en'
+            print("English selected.")
+            lanCheck == 1
+
+        if lanInt == 2:
+            lanVal = 'it'
+            print("Lingua Italiana selezionata.")
+            lanCheck == 1
+
+
+        if lanInt == 3:
+            lanVal = 'no'
+            print("Norsk valgt.")
+            lanCheck == 1
+
+        else:
+            print('Invalid number!')
+
+
+
+
+lanInput()
+print("lanInput exited properly.")
+
 
 # Trying to figure out how to use GetText...
-lanFile = 'idk'
+# Maybe NullTranslations can be useful?
+# Screw this, I`ll do it my own way
+# Totally off topic, but in Python 2.7 you could execute some pretty dangerous commands through the input function, if running through Sudo. For example you could remove OS files.
+
 
 

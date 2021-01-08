0b2373d3f8820ad329a8d4528e996ac2e8d50a58
dariopedroni_dev <dario.pedroni80@gmail.com>
2021-01-08 19:39:26 +0100

    Switched from PlaySound to Pyglet for gapless audio playback. Now focusing on gaplessPlayback.py.

--- gaplessPlayback.py
+++ gaplessPlayback.py
@@ -1,15 +1,13 @@
-
-#music = pyglet.resource.media('music/wishloop.mp3', streaming=False)
-#player = pyglet.media.Player()
-#player.EOS_LOOP = 'loop'
-#music.play()
-
-# I swear, this shit is more complicated than Neon Genesis Evangelion
 import pyglet
 from pyglet.media import SourceGroup
-
+# I swear, this shit is more complicated than Neon Genesis Evangelion
+player = pyglet.media.Player()
 source1 = pyglet.resource.media('music/wishloop.mp3')
 source_group = SourceGroup()
 source_group.add(source1)
 player.queue(source_group)
+player.loop = True
+player.play()
+
+
 pyglet.app.run()
